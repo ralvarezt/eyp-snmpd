44047dc812e8dda806b90f63e765d66830d7cb2b
Jordi Prats <jprats@systemadmin.es>
2017-10-06 15:15:03 +0200

    snmpd absent

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,12 +4,14 @@
 #
 class snmpd::config inherits snmpd {
 
-  file { '/etc/snmp/snmpd.conf':
-    ensure  => 'present',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => template("${module_name}/snmpdconf.erb"),
+  if($snmpd::manage_package and $snmpd::package_ensure!='absent')
+  {
+    file { '/etc/snmp/snmpd.conf':
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => template("${module_name}/snmpdconf.erb"),
+    }
   }
-
 }
