8f706d210871e2c0513a15fc393308fe331768cb
Raúl Álvarez Tenor <raul.alvarez@nttcom.ms>
2020-02-19 17:14:49 +0100

    Preliminary work, using snmp3_user functions from puppet-snmp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,5 @@
 class snmpd::params {
 
-  $service_name = 'snmpd'
-
   case $::osfamily
   {
     'redhat':
@@ -16,26 +14,5 @@ class snmpd::params {
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
       }
     }
-    'Debian':
-    {
-      $package_name = 'snmpd'
-
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
-        }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
-      }
-    }
-    default: { fail('Unsupported OS!')  }
   }
 }
